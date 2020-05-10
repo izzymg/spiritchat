55bd4d563237266f478f9f69f9ccacc5938bd276
izzymg <izzymhg@gmail.com>
2020-05-11 04:03:31 +1200

    Trim excess newlines off of post content

--- data/validation.go
+++ data/validation.go
@@ -2,14 +2,30 @@ package data
 
 import (
 	"html"
+	"regexp"
 	"strings"
 )
+
+// Replace 3 or more newlines, including possible spaces
+var newlines = regexp.MustCompile("(\n\\s*){3,}")
+
+// Replace all carriage returns with normal newlines
+var carriageReturns = regexp.MustCompile("\r\n")
 
 /*
 CheckContent validates a post's contents, returning the content sanitized as
 the first argument, or a human-readable error message as the second. */
 func CheckContent(content string) (string, string) {
-	content = strings.TrimSpace(html.EscapeString(content))
+	content = strings.TrimSpace(
+		html.EscapeString(
+			strings.ToValidUTF8(
+				content,
+				"",
+			),
+		),
+	)
+	content = carriageReturns.ReplaceAllString(content, "\n")
+	content = newlines.ReplaceAllString(content, "\n")
 	if len(content) < minContentLen || len(content) > maxContentLen {
 		return "", InvalidContentLen
 	}
--- data/validation_test.go
+++ data/validation_test.go
@@ -1,6 +1,9 @@
 package data
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 // Generates string of "a" n times
 func genStr(n int, s string) string {
@@ -40,5 +43,24 @@ func TestCheckContent(t *testing.T) {
 	_, errStr = CheckContent("   a   ")
 	if len(errStr) == 0 {
 		t.Fatal("Expected an err string")
+	}
+
+	ret, errStr := CheckContent("\rxxz\r \r\n  \r")
+	if len(errStr) > 0 {
+		t.Fatal("Expected no err string")
+	}
+	if strings.ContainsAny(ret, "\r\n") {
+		t.Fatal("Expected no return chars")
+	}
+	if strings.ContainsAny(ret, "\r") {
+		t.Fatal("Expected no return chars")
+	}
+
+	ret, errStr = CheckContent("dog\n cat \n\n tiger \n\n\n\n\n bat")
+	if len(errStr) > 0 {
+		t.Fatal("Expected no err string")
+	}
+	if c := strings.Count(ret, "\n"); c != 4 {
+		t.Fatalf("Expected 3 newlines, got %d", c)
 	}
 }
